b3a60ff2af839df6e88ce8b4b89cb5c40606ebad
javi <javicast3@gmail.com>
2018-10-16 11:32:03 -0300

    Emit newMessage oncreateMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,11 +3,6 @@ var socket = io();
 socket.on('connect', function () {
   console.log("connected to server");
 
-  socket.emit('createMessage', {
-    from: 'javi',
-    text: 'yup, that works for me'
-  });
-
 });
 socket.on('disconnect', function () {
   console.log("disconnected from server");
